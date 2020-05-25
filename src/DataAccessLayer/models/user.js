76dfb728a6dd064fe6d430f9c6a081f7e5955d76
frankly034 <franklynugege@gmail.com>
2020-05-25 12:45:06 +0100

    feat(sequelize): add sequelize - add sequelize config

---
+++ src/DataAccessLayer/models/user.js
@@ -0,0 +1,28 @@
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {});
+  User.associate = () => {
+    // associations can be defined here
+  };
+  return User;
+};
+
+export default user;
